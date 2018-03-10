99ba9e091e05b1bd544cc2ba8051e3e61153cd93
yukunfg <yukunfg@gmail.com>
2018-03-10 02:47:03 -0500

    add export LANG for gbk

--- env_set.sh
+++ env_set.sh
@@ -11,7 +11,7 @@ alias l='ls -lht'
 set -o vi
 
 # Set LANG
-export LANG=zh_CN.GBK
+# export LANG=zh_CN.GBK
 
 # Set local path probably used in future
 # export LD_LIBRARY_PATH="$HOME/local/lib/:$LD_LIBRARY_PATH"
