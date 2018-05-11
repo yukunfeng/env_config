639dae37c3fd8e0a5dd289bc36b3e5c53d48516c
yukunfeng <yukunfg@gmail.com>
2018-05-11 23:37:25 +0900

    add set_env.sh

---
+++ set_env.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# ----------------------------------------------------------------------
+# Author:	Yukun Feng
+# Date:	05-11-18
+# Email:	yukunfg@gmail.com
+# Description:	 Copy .vimrc, .zshrc and snippts to home
+# ----------------------------------------------------------------------
+
+cp -r .vimrc .zshrc myultisnips $HOME/.vim
