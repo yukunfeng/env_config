c5011ffbf9e7180a76abe887831313f5a1bfd571
YUKUN FENG <yukun@tsumire.lrlab>
2018-07-06 15:11:13 +0900

    ipython command codes

--- ipython_common_import.py
+++ ipython_common_import.py
@@ -1,3 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
+
+x = torch.tensor([[0.1, 1.2], [2.2, 3.1], [4.9, 5.2]], requires_grad=True, device="cpu")
+y = torch.randint(0, 5, (2, 3, 4))
