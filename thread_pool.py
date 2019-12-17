cc5b410cd3b646698ec9a27c0decba66a4263846
Yukun Feng <yukunfg@google.com>
2019-12-17 11:04:38 -0800

    example of thread pool

---
+++ thread_pool.py
@@ -0,0 +1,26 @@
+from multiprocessing.pool import ThreadPool
+
+def foo(word, number):
+    #  print word*number
+    return number
+
+def starfoo(args):
+    """ 
+
+    We need this because map only supports calling functions with one arg. 
+    We need to pass two args, so we use this little wrapper function to
+    expand a zipped list of all our arguments.
+
+    """    
+    return foo(*args)
+
+words = ['hello', 'world', 'test', 'word', 'another test']
+numbers = [1,2,3,4,5]
+pool = ThreadPool(2)
+# We need to zip together the two lists because map only supports calling functions
+# with one argument. In Python 3.3+, you can use starmap instead.
+results = pool.map(starfoo, zip(words, numbers))
+print(results)
+
+pool.close()
+pool.join()
