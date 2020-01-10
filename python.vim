6802841258d1306e6e5142bb98de9d4458d81cec
Yukun Feng <yukunfg@google.com>
2020-01-09 16:42:35 -0800

    set after-indent

---
+++ python.vim
@@ -0,0 +1,5 @@
+" the best explanation about tab in vim I have seen is http://tedlogan.com/techblog3.html
+set expandtab        " in Insert mode: Use the appropriate number of spaces to insert a <Tab>.
+set tabstop=4        " number of spaces that a <Tab> in the file counts for
+set shiftwidth=4     " number of spaces to use for each step of (auto)indent
+set softtabstop=4	" number of spaces that a <Tab> counts for while performing editing operations
--- set_env.sh
+++ set_env.sh
@@ -7,7 +7,15 @@
 # Description:	 Copy .vimrc, .zshrc and snippts to home
 # ----------------------------------------------------------------------
 
+set -x
+
+
 cp -r myultisnips $HOME/.vim
 cp .vimrc .zshrc .tmux.conf $HOME
 cp ./refined.zsh-theme $HOME/.oh-my-zsh/custom/themes
 cp ./auto-pairs.vim $HOME/.vim/plugged/auto-pairs/plugin
+
+# setting for indent (avoid overriding by other plugins)
+after_plugin_dir="$HOME/.vim/after/ftplugin/"
+mkdir -p "$after_plugin_dir"
+cp ./python.vim $after_plugin_dir
